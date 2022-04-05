c32dcae3afa97dbe9b88950b59524c9ea0a878b7
SasiKalyan <kanakamsasikalyan1@gmail.com>
2022-04-05 19:09:55 +0530

    Updated

---
+++ Method-Overriding/SuperKeyword_MethodOveriding.java
@@ -0,0 +1,24 @@
+class ABC{
+   public void myMethod()
+   {
+	System.out.println("Overridden method");
+   }	   
+}
+class Demo extends ABC{
+   public void myMethod(){
+	//This will call the myMethod() of parent class
+	super.myMethod();
+	System.out.println("Overriding method");
+   }
+   public static void main( String args[]) {
+	Demo obj = new Demo();
+	obj.myMethod();
+   }
+}
+
+/*
+Output:
+
+Class ABC: mymethod()
+Class Test: mymethod()
+*/
